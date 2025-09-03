63c38bd5c318135fb7623f356cfda00afdc9f8b6
devonZeuz <devonZeuz@users.noreply.github.com>
2025-09-03 23:16:39 +0200

    React .env

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL =
+  process.env.REACT_APP_BASE_URL || "http://localhost:8000";
 
 // utils/apiPath.js
 export const API_PATHS = {
